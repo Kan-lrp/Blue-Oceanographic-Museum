b36755e11168b1874a00679fb6f9931e27fddb47
梁 <66423949+Kan-lrp@users.noreply.github.com>
2023-09-04 10:56:50 +0800

    Add files via upload

---
+++ js/Questions.js
@@ -0,0 +1,105 @@
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+
+var QuestionJosn = [{
+    "questionId": "1",
+    "questionTitle": "在国家海洋博物馆一楼的海洋文化主题空间中，有一个巨大的古代帆船模型属于以下哪一种古船类型？",
+    "questionItems": " 广船; 沙船;鸟船;福船;",
+    "questionAnswer": "c"
+}, {
+    "questionId": "2",
+    "questionTitle": "在地质时代划分中，以下哪个纪属于中生代？？ ",
+    "questionItems": "寒武纪;泥盆纪;二叠纪;侏罗纪;催乳素",
+    "questionAnswer": "b"
+}, {
+    "questionId": "3",
+    "questionTitle": "（ ）是中国第一条能在极地海区破冰前行的船只。 ",
+    "questionItems": "雪龙号;辽宁号;蛟龙号;科学号",
+    "questionAnswer": "d"
+}, {
+    "questionId": "4",
+    "questionTitle": "下列生物中哪种是哺乳动物？ ",
+    "questionItems": "原鲸;鱼龙;海百合;恐鱼;",
+    "questionAnswer": "d"
+}, {
+    "questionId": "5",
+    "questionTitle": "以下对翼龙描述正确的一项是（ ） ",
+    "questionItems": "翼龙是恐龙的一种;翼龙是一种长得像恐龙的鸟类;翼龙是一种会飞的爬行动物;翼龙是一种会飞的哺乳动物;",
+    "questionAnswer": "b"
+}, {
+    "questionId": "6",
+    "questionTitle": "两个起源于不同祖先的物种，由于生活环境的相似，进化出了相近的体态，这种现象被称为 ",
+    "questionItems": "生物协同进化;生物趋同进化;生物趋异进化，未知;",
+    "questionAnswer": "c"
+}, {
+    "questionId": "7",
+    "questionTitle": "过去，人们在海上遇险，就使用“（ ）”呼救信号。",
+    "questionItems": "DNA;SOS;NANS;SOA;",
+    "questionAnswer": "a"
+}, {
+    "questionId": "8",
+    "questionTitle": "在寒武纪相关化石中，（ ）的化石占到了60%，是当时海洋中 ",
+    "questionItems": "三叶虫，海百合;鹦鹉螺;奇虾;",
+    "questionAnswer": "d"
+}, {
+    "questionId": "9",
+    "questionTitle": "我们平时经常会见到多姿多彩的贝壳，那你知道贝类属于哪种海洋动物吗？",
+    "questionItems": "节肢动物;软体动物;腕足动物;腔肠动物;",
+    "questionAnswer": "e"
+}, {
+    "questionId": "10",
+    "questionTitle": "当月亮运行到太阳和地球中间，我们在地球上可以看到什么样的天象？ ",
+    "questionItems": "月食;日食;凌日;行星连珠;",
+    "questionAnswer": "b"
+}, {
+    "questionId": "11",
+    "questionTitle": "在以下几个选项人物中，被当地人们奉为海神的是（ ） ",
+    "questionItems": "波塞冬;妈祖;埃吉尔;以上都是;",
+    "questionAnswer": "c"
+}, {
+    "questionId": "12",
+    "questionTitle": "我国大陆海岸线北起鸭绿江口，南至北仑河口，总长度约（ ）千米",
+    "questionItems": "1.5万;1.6万;1.8万;2.1万;",
+    "questionAnswer": "d"
+}, {
+    "questionId": "13",
+    "questionTitle": "“隔墙”上都有一个小孔，连接起来就像是贯通许多房间的一条走廊？ ",
+    "questionItems": "海百合;菊石;鹦鹉螺;板足鲎;",
+    "questionAnswer": "a"
+}, {
+    "questionId": "14",
+    "questionTitle": "海洋面积占地球表面积的近（ ） ",
+    "questionItems": "2％;51%;71%;91%;",
+    "questionAnswer": "a"
+}, {
+    "questionId": "15",
+    "questionTitle": "促进机体产热增加的主要激素是 ",
+    "questionItems": "胰岛素;生长素;甲状腺激素;肾上腺素; 糖皮质激素",
+    "questionAnswer": "c"
+}, {
+    "questionId": "16",
+    "questionTitle": "在人类和哺乳动物，对骨和脑的发育尤为重要的激素是 ",
+    "questionItems": "生长素;糖皮质激素;盐皮质激素;甲状腺激素;甲状旁腺激素",
+    "questionAnswer": "d"
+}, {
+    "questionId": "17",
+    "questionTitle": "地方性甲状腺肿的主要发病原因是 ",
+    "questionItems": "由于促甲状腺素分泌过少;甲状腺合成的甲状腺激素过多;食物中缺少钙和蛋白质;食物中缺少酪氨酸;食物中缺少碘",
+    "questionAnswer": "e"
+}, {
+    "questionId": "18",
+    "questionTitle": "关于甲状腺激素正确的论述是 ",
+    "questionItems": "属于类固醇激素;合成后贮存于细胞内;T4的活性比T3大;可促进生长和发育过程;幼年缺乏时可导致侏儒症",
+    "questionAnswer": "d"
+}, {
+    "questionId": "19",
+    "questionTitle": "人体内储存量最多的激素是 ",
+    "questionItems": "生长素;胰岛素;甲状腺素;促肾上腺皮质激素;肾上腺素和去甲肾上腺素",
+    "questionAnswer": "c"
+}, {
+    "questionId": "20",
+    "questionTitle": "对胰岛素的叙述错误的是 ",
+    "questionItems": "由胰岛B细胞分泌;可使血糖浓度下降;迷走神经兴奋可使其分泌减少;血糖浓度升高可促进其分泌;胃肠激素可促进其分泌",
+    "questionAnswer": "c"
+}];
